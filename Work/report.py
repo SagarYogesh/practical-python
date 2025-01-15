2ca279761f91c328c230e549208deb2de555fe8e
SagarYogesh <sagar.yogeesh@gmail.com>
2025-01-15 22:45:39 +0530

    Completed till Exercise 2.6

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,38 @@
 # report.py
 #
 # Exercise 2.4
+
+import csv
+
+def read_portfolio(filename):
+    portfolio = []
+
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        next(rows)
+        for row in rows:
+            try:
+                holding = {
+                  'name':row[0],
+                  'shares':int(row[1]),
+                  'price':float(row[2])
+                  }
+                portfolio.append(holding)
+            except ValueError:
+                print("Couldn't parse", row)
+
+    return portfolio
+
+def read_prices(filename):
+    prices = {}
+
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        for row in rows:
+            try:
+                print(row)
+                prices[row[0]] = float(row[1])
+            except:
+                print("Couldn't parse", row)
+
+    return prices
