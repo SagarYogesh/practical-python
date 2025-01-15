68fb022e31abe25916529c83d6c6d6b8d2df3782
SagarYogesh <sagar.yogeesh@gmail.com>
2025-01-15 13:57:33 +0530

    Bouncing ball height program v0
    Wrote program to calculate height of bounce of ball after each bounce

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,9 @@
 # bounce.py
 #
 # Exercise 1.5
+drop_height = 100 # Meters
+
+for i in range(10):
+    bounce_height = round((3 * drop_height)/5, 4) # Meters
+    print("Bounce height:", bounce_height)
+    drop_height = bounce_height
