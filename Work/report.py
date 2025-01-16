b0a101bf59cab3bc0e1ba1a545ef18d255842b71
SagarYogesh <sagar.yogeesh@gmail.com>
2025-01-16 17:35:29 +0530

    Created make_report function. Updated code to use zip()

--- Work/report.py
+++ Work/report.py
@@ -4,19 +4,35 @@
 
 import csv
 
+def make_report(portfolio, prices):
+    report = portfolio
+
+    for row in report:
+        try:
+            change = prices[row['name']] - float(row['price'])
+            row['price'] = float(prices[row['name']])
+            row['change'] = change
+        except:
+            print('Cound not execute')
+
+    headers = ('Name', 'Shares', 'Price', 'Change')
+    print(f'{headers[0]:>10s}{headers[1]:>10s}{headers[2]:>10s}{headers[3]:>10s}')
+    print('---------- ---------- ---------- ---------- ')
+    for row in report:
+        price = '${:.2f}'.format(row['price'])
+        print(f'{row['name']:>10s}{row['shares']:>10s}{price:>10}{row['change']:>10.2f}')
+
+
+
 def read_portfolio(filename):
     portfolio = []
 
     with open(filename, 'rt') as f:
         rows = csv.reader(f)
-        next(rows)
+        columns = next(rows)
         for row in rows:
             try:
-                holding = {
-                  'name':row[0],
-                  'shares':int(row[1]),
-                  'price':float(row[2])
-                  }
+                holding = dict(zip(columns, row))
                 portfolio.append(holding)
             except ValueError:
                 print("Couldn't parse", row)
@@ -30,9 +46,12 @@ def read_prices(filename):
         rows = csv.reader(f)
         for row in rows:
             try:
-                print(row)
                 prices[row[0]] = float(row[1])
             except:
                 print("Couldn't parse", row)
 
     return prices
+
+portfolio = read_portfolio('Data/portfoliodate.csv')
+prices = read_prices('Data/prices.csv')
+make_report(portfolio, prices)
