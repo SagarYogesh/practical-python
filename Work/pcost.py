bcae9e494a99767f77546ecca9a1ae65200c5e8a
SagarYogesh <sagar.yogeesh@gmail.com>
2025-01-17 17:43:31 +0530

    Used function read_portfolio from report.py to read. Added main module

--- Work/pcost.py
+++ Work/pcost.py
@@ -3,24 +3,13 @@
 # Exercise 1.27
 
 import sys
-import csv
+from report import read_portfolio
 
 def portfolio_cost(filename):
     '''Computes the total cost (shares*price) of a portfolio file'''
-    TotalCost = 0.0
+    portfolio = read_portfolio(filename)
 
-    with open(filename, 'rt') as f:
-        rows = csv.reader(f)
-        headers = next(rows)
-        for rownum, row in enumerate(rows, start = 1):
-            record = dict(zip(headers, row))
-            try:
-                nshares = int(record['shares'])
-                price = float(record['price'])
-                TotalCost += nshares * price
-            except ValueError:
-                print(f'Row {rownum}: Couldn\'t parse {row}')
-    return TotalCost
+    return sum([row['shares']*row['price'] for row in portfolio])
 
 if len(sys.argv) == 2:
     filename = sys.argv[1]
@@ -29,3 +18,12 @@ else:
 
 cost = portfolio_cost(filename)
 print(f'Total Cost = {cost}')
+
+def main(args):
+    if len(args) != 2:
+        raise SystemExit('Usage: %s portfile' % args[0])
+    portfolio_cost(args[1])
+
+if __name__ == '__main__':
+    import sys
+    main(sys.argv)
