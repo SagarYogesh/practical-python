8460a1d8f89e7f59320fe5c1e4846c065b229194
SagarYogesh <sagar.yogeesh@gmail.com>
2025-01-17 14:09:07 +0530

    Added comments to functions. Reorganized functions.

--- Work/report.py
+++ Work/report.py
@@ -4,27 +4,12 @@
 
 import csv
 
-def make_report(portfolio, prices):
-    report = portfolio
+def read_portfolio(filename) -> list:
+    '''
+    Read a stock portfolio file into a list of dictionaries with keys
+    name, shares, and price.
+    '''
 
-    for row in report:
-        try:
-            change = prices[row['name']] - float(row['price'])
-            row['price'] = float(prices[row['name']])
-            row['change'] = change
-        except:
-            print('Cound not execute')
-
-    headers = ('Name', 'Shares', 'Price', 'Change')
-    print(f'{headers[0]:>10s}{headers[1]:>10s}{headers[2]:>10s}{headers[3]:>10s}')
-    print('---------- ---------- ---------- ---------- ')
-    for row in report:
-        price = '${:.2f}'.format(row['price'])
-        print(f'{row['name']:>10s}{row['shares']:>10s}{price:>10}{row['change']:>10.2f}')
-
-
-
-def read_portfolio(filename):
     portfolio = []
 
     with open(filename, 'rt') as f:
@@ -44,7 +29,12 @@ def read_portfolio(filename):
 
     return portfolio
 
-def read_prices(filename):
+
+def read_prices(filename) -> dict:
+    '''
+    Reads given file for name and share price
+    '''
+
     prices = {}
 
     with open(filename, 'rt') as f:
@@ -57,6 +47,33 @@ def read_prices(filename):
 
     return prices
 
-portfolio = read_portfolio('Data/portfoliodate.csv')
-prices = read_prices('Data/prices.csv')
-make_report(portfolio, prices)
+def make_report(portfolio, prices) -> list:
+    '''
+    Calculates change in share price and prints output report with Name, no.of shares, price and change
+    '''
+    for row in portfolio:
+        try:
+            change = prices[row['name']] - float(row['price'])
+            row['price'] = float(prices[row['name']])
+            row['change'] = change
+        except:
+            print('Cound not execute')
+
+    return portfolio
+
+
+def portfolio_report(portfolio_filename, prices_filename):
+    '''
+    Prints report of name, shares, price and change in price of given stock portfolio file and latest price file
+    '''
+
+    portfolio = read_portfolio(portfolio_filename)
+    prices = read_prices(prices_filename)
+    report = make_report(portfolio, prices)
+
+    headers = ('Name', 'Shares', 'Price', 'Change')
+    print(f'{headers[0]:>10s}{headers[1]:>10s}{headers[2]:>10s}{headers[3]:>10s}')
+    print('---------- ---------- ---------- ---------- ')
+    for row in report:
+        price = '${:.2f}'.format(row['price'])
+        print(f'{row['name']:>10s}{row['shares']:>10s}{price:>10}{row['change']:>10.2f}')
