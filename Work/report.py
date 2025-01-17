a614408bc4106fea5ba0fd0870f35473df859cdc
SagarYogesh <sagar.yogeesh@gmail.com>
2025-01-18 00:28:07 +0530

    OOPS + table format
    Updated with OOPS + tableformat

--- Work/report.py
+++ Work/report.py
@@ -3,44 +3,50 @@
 # Exercise 2.4
 
 import fileparse
+from stock import Stock
+import tableformat
 
 def read_portfolio(filename):
     '''
     Read a stock portfolio file into a list of dictionaries with keys
     name, shares, and price.
     '''
-    with open(filename, 'rt') as file:
-        return fileparse.parse_csv(file, select=['name','shares','price'], types=[str,int,float], has_headers=True)
+    with open(filename) as lines:
+        portdicts = fileparse.parse_csv(lines,
+                                        select=['name','shares','price'],
+                                        types=[str,int,float], has_headers=True)
+
+    portfolio = [ Stock(d['name'], d['shares'], d['price']) for d in portdicts ]
+    return portfolio
 
 def read_prices(filename):
     '''
     Read a CSV file of price data into a dict mapping names to prices.
     '''
-    with open(filename, 'rt') as file:
-        return dict(fileparse.parse_csv(filename,types=[str,float], has_headers=False))
+    with open(filename) as lines:
+        return dict(fileparse.parse_csv(lines, types=[str,float], has_headers=False))
 
-def make_report_data(portfolio,prices):
+def make_report_data(portfolio, prices):
     '''
     Make a list of (name, shares, price, change) tuples given a portfolio list
     and prices dictionary.
     '''
     rows = []
-    for stock in portfolio:
-        current_price = prices[stock['name']]
-        change = current_price - stock['price']
-        summary = (stock['name'], stock['shares'], current_price, change)
+    for s in portfolio:
+        current_price = prices[s.name]
+        change = current_price - s.price
+        summary = (s.name, s.shares, current_price, change)
         rows.append(summary)
     return rows
 
-def print_report(reportdata):
+def print_report(reportdata, formatter):
     '''
     Print a nicely formated table from a list of (name, shares, price, change) tuples.
     '''
-    headers = ('Name','Shares','Price','Change')
-    print('%10s %10s %10s %10s' % headers)
-    print(('-'*10 + ' ')*len(headers))
-    for row in reportdata:
-        print('%10s %10d %10.2f %10.2f' % row)
+    formatter.headings(['Name','Shares','Price','Change'])
+    for name, shares, price, change in reportdata:
+        rowdata = [ name, str(shares), f'{price:0.2f}', f'{change:0.2f}' ]
+        formatter.row(rowdata)
 
 def portfolio_report(portfoliofile, pricefile):
     '''
@@ -54,7 +60,9 @@ def portfolio_report(portfoliofile, pricefile):
     report = make_report_data(portfolio, prices)
 
     # Print it out
-    print_report(report)
+#    formatter = tableformat.CSVTableFormatter()
+    formatter = tableformat.HTMLTableFormatter()
+    print_report(report, formatter)
 
 def main(args):
     if len(args) != 3:
