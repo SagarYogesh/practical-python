ac41728648ae84db96ff6cbb72f66fe47660eb55
SagarYogesh <sagar.yogeesh@gmail.com>
2025-01-18 00:28:32 +0530

    OOPS trial for stocks
    OOPS implementation

---
+++ Work/stock.py
@@ -0,0 +1,23 @@
+'''
+Exercise 4 OOPS
+'''
+class Stock:
+    def __init__(self, name, shares, price):
+        self.name = name
+        self.shares = shares
+        self.price = price
+
+    def cost(self):
+        return self.shares * self.price
+
+    def sell(self, nshares):
+        self.shares -= nshares
+
+class MyStock(Stock):
+    def __init__(self, name, shares, price, factor):
+        # Check the call to `super` and `__init__`
+        super().__init__(name, shares, price)
+        self.factor = factor
+
+    def cost(self):
+        return self.factor * super().cost()
